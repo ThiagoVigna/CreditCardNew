556ac7522d912ee367df9f4a4722ed62e10a6836
thiagovigna <thiagovigna@hotmail.com>
2021-11-09 15:40:32 -0300

    Adicionar arquivos de projeto.

---
+++ Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Client
+{
+  class Program
+    {
+        private static string PublicKeyLocation = @"C:\Users\PCHome\source\repos\AsymetricEncryption.JWE\CreditCardEncryption\bin\Debug\net5.0\publickey.json";
+        private static HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = await Loadkey();
+
+            var enc = new EncryptingCredentials(key, SecurityAlgorithms.RsaOAEP, SecurityAlgorithms.Aes128CbcHmacSha256);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+              Issuer = "CartãoVirtualTH",
+              Audience = "cartão credito",
+              Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("cc", "4000-0000-0000-0002"),
+                }),
+                EncryptingCredentials = enc
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            Console.WriteLine(tokenHandler.WriteToken(token));
+        }
+
+        private static async Task<SecurityKey> Loadkey()
+        {
+            var publicKey = await client.GetStringAsync("https://localhost:5001/jwks_e");
+            var key = JsonWebKeySet.Create(publicKey);
+            return key.Keys.FirstOrDefault();
+        }
+    }
+}
---
+++ ServerApi/Controllers/ServerApiController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using NetDevPack.Security.Jwt.Interfaces;
+
+namespace ServerApi.Controllers
+{
+  [ApiController]
+    [Route("teste")]
+    public class ServerApiController : ControllerBase
+    {
+        private readonly ILogger<ServerApiController> _logger;
+        private readonly IJsonWebKeySetService _jwksService;
+
+        public ServerApiController(ILogger<ServerApiController> logger, IJsonWebKeySetService jsonWebKeySetService)
+        {
+            _logger = logger;
+            _jwksService = jsonWebKeySetService;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string jwe)
+        {
+            var handler = new JsonWebTokenHandler();
+            var encryptingCredentials = _jwksService.GetCurrentEncryptingCredentials();
+            var result = handler.ValidateToken(jwe,
+                new TokenValidationParameters
+                {
+                    ValidIssuer = "CartãoVirtualTH",
+                    ValidAudience = "cartão credito",
+                    RequireSignedTokens = false,
+                    TokenDecryptionKey = encryptingCredentials.Key,
+                });
+            if (!result.IsValid)
+                BadRequest();
+
+            return Ok(result.Claims);
+        }
+    }
+
+}
